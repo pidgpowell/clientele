81aa2d8287ad42b55b015f9e0a4c00ee6bea99e0
Mark Powell <longlostcousin@gmail.com>
2022-05-11 12:58:25 -0400

    Basic show page added

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<script setup>
+const { data: home } = await useAsyncData("static", () => {
+  return queryContent("/").findOne();
+});
+
+const formatDate = useFormat();
+</script>
+
+<template>
+  <div>
+    <NuxtLayout>
+      <div
+        v-if="home"
+        class="-mt-6 md:-mt-12 xl:-mt-[5%] ml-[3.4vw] py-2 pl-6 md:pl-[3.7vw] border-l sm:border-l-2 border-gray-100 prose md:max-w-[59vw]"
+      >
+        <p class="date text-xs text-gray-500">{{ formatDate(home.date) }}</p>
+        <h1 class="mt-0 mb-0">{{ home.title }}</h1>
+        <Content v-if="home" :document="home" />
+      </div>
+    </NuxtLayout>
+  </div>
+</template>
---
+++ pages/shows.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h2 class="text-3xl">Shows</h2>
+
+    <div class="p-4">
+      <ul>
+        <li>Show1</li>
+      </ul>
+    </div>
+  </div>
+</template>
