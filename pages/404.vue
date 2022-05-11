84e8563436dd7676368f27366251ccad3205779e
Mark Powell <longlostcousin@gmail.com>
2022-05-11 14:04:05 -0400

    WIP

---
+++ pages/404.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <p>That page doesn't exist</p>
+  </div>
+</template>
--- pages/shows/[...year].vue
+++ pages/shows/[...year].vue
@@ -1,18 +1,19 @@
 <script setup>
 const { data: shows } = await useAsyncData("shows", () => {
-  return queryContent("/shows").find();
+  return queryContent("/shows").sortBy("date", "desc").find();
 });
 const formatDate = useFormat();
 </script>
 
 <template>
   <div>
-    <h2 class="text-3xl">Shows</h2>
+    <h2 class="text-3xl">Shows {{ $route.params.year }}</h2>
 
     <div class="p-4">
       <div class="flex flex-col gap-4">
         <div v-for="(show, index) in shows" :key="index">
-          {{ formatDate(show.date) }} {{ show.city }}
+          {{ formatDate(show.date) }} – {{ show.city }} – {{ show.venue }}
+          {{ show.description }}
         </div>
       </div>
     </div>
