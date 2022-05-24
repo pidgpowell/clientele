0323569a93ed24ee4846e9931e7f6775b8162679
Mark Powell <longlostcousin@gmail.com>
2022-05-24 19:45:27 -0400

    WIP

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ import { defineNuxtConfig } from "nuxt";
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   buildModules: [
-    '@nuxt/content-edge',
+    '@nuxt/content',
     '@nuxtjs/tailwindcss'
   ]
 });
