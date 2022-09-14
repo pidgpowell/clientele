25ad4d007cba2a52202811ccda83f8e20584d3dc
Mark Powell <longlostcousin@gmail.com>
2022-09-14 19:17:41 -0400

    nuxt rc10 update

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,15 +1,6 @@
-import { defineNuxtConfig } from "nuxt";
-
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   ssr: true,
-
-  nitro: {
-    compressPublicAssets: true,
-    prerender: {
-      crawlLinks: true
-    }
-  },
 
   modules: ["@nuxt/content", "@nuxtjs/tailwindcss"],
 });
