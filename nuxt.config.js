48b1b6a2567f7ee87f0af7691a01cbf8d031daff
Mark Powell <longlostcousin@gmail.com>
2023-06-08 18:11:14 -0400

    dependency updates

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,12 +1,15 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   ssr: true,
-
   modules: ["@nuxt/content", "@nuxtjs/tailwindcss"],
 
   vue: {
     compilerOptions: {
       isCustomElement: (tag) => ['lite-youtube'].includes(tag),
     },
+  },
+
+  devtools: {
+    enabled: false
   }
 });
