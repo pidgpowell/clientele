fb84929f316af376a2a86b477af97aa2a1cbcb8b
Mark Powell <longlostcousin@gmail.com>
2023-07-14 21:53:01 -0500

    use nuxt/image module (#5)
    * Test nuxt images
    
    * Update 2023-04-24-i-am-not-there-anymore.md
    
    * Update tailwind.css
    
    ---------
    
    Co-authored-by: Mark Powell <mpowell@atomicsoftware.com>

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,16 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   ssr: true,
-  modules: ["@nuxt/content", "@nuxtjs/tailwindcss"],
+
+  modules: [
+    "@nuxt/content",
+    "@nuxt/image",
+    "@nuxtjs/tailwindcss"
+  ],
+
+  image: {
+    domains: ['pbs.twimg.com', 'dispatch-public.s3.amazonaws.com']
+  },
 
   experimental: {
     payloadExtraction: true
