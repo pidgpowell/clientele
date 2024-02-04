77cd14ce4f4a46c7dd8b84ec77fd354479370306
Mark Powell <longlostcousin@gmail.com>
2024-02-04 08:28:58 -0500

    Formatting updates

--- components/AppNavigation.vue
+++ components/AppNavigation.vue
@@ -1,15 +1,18 @@
 <script setup>
-defineProps({
-  navigationTree: {
-    type: Array,
-    default: () => [],
-  },
-});
+  defineProps({
+    navigationTree: {
+      type: Array,
+      default: () => [],
+    },
+  });
 </script>
 
 <template>
   <ul class="navigation">
-    <li v-for="(item, index) in navigationTree" :key="index">
+    <li
+      v-for="(item, index) in navigationTree"
+      :key="index"
+    >
       <NuxtLink :to="item._path">
         {{ item.title }}
       </NuxtLink>
@@ -23,11 +26,11 @@ defineProps({
 </template>
 
 <style scoped>
-.navigation li {
-  list-style-type: "_ ";
-}
+  .navigation li {
+    list-style-type: "_ ";
+  }
 
-.navigation ul {
-  margin-left: 0.8rem;
-}
+  .navigation ul {
+    margin-left: 0.8rem;
+  }
 </style>
