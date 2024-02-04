2f0f9986e214e0bc3b86889275a1fc22e7e88288
Mark Powell <longlostcousin@gmail.com>
2024-02-04 08:23:59 -0500

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,69 +1,69 @@
 <script setup>
-	const { data } = await useAsyncData("news-home", () =>
-		queryContent("news").sort({ date: -1 }).limit(1).find()
-	);
-	const formatDate = useDateFormat();
-	const excerpt = true;
+  const { data } = await useAsyncData("news-home", () =>
+    queryContent("news").sort({ date: -1 }).limit(1).find()
+  );
+  const formatDate = useDateFormat();
+  const excerpt = true;
 
-	useHead({
-		title: "",
-	});
+  useHead({
+    title: "",
+  });
 </script>
 
 <template>
-	<div class="relative pt-2">
-		<nav
-			class="absolute -top-2 flex items-center text-xs text-gray-500"
-			aria-label="breadcrumbs"
-		>
-			<icon-home /> Home
-		</nav>
+  <div class="relative pt-2">
+    <nav
+      class="absolute -top-2 flex items-center text-xs text-gray-500"
+      aria-label="breadcrumbs"
+    >
+      <icon-home /> Home
+    </nav>
 
-		<div
-			v-for="post in data"
-			:key="post._id"
-		>
-			<h2>
-				<NuxtLink
-					:to="post._path"
-					class="no-underline hover:underline"
-				>
-					<span v-html="post.title" />
-				</NuxtLink>
-			</h2>
+    <div
+      v-for="post in data"
+      :key="post._id"
+    >
+      <h2>
+        <NuxtLink
+          :to="post._path"
+          class="no-underline hover:underline"
+        >
+          <span v-html="post.title" />
+        </NuxtLink>
+      </h2>
 
-			<p class="text-gray-500 dark:text-gray-400 mb-0 !text-sm">
-				{{ formatDate(post.date, { dateStyle: "medium" }) }}
-			</p>
+      <p class="text-gray-500 dark:text-gray-400 mb-0 !text-sm">
+        {{ formatDate(post.date, { dateStyle: "medium" }) }}
+      </p>
 
-			<ContentRenderer
-				:value="post"
-				:excerpt="excerpt"
-				class="prose-table:max-w-sm"
-			/>
+      <ContentRenderer
+        :value="post"
+        :excerpt="excerpt"
+        class="prose-table:max-w-sm"
+      />
 
-			<NuxtLink
-				v-if="excerpt && post.excerpt"
-				:to="post._path"
-				class="border-1 px-4 py-3 border-black border-solid border dark:border-gray-600 rounded-md no-underline hover:bg-gray-100 dark:hover:bg-gray-800"
-			>
-				Read more
-			</NuxtLink>
-		</div>
+      <NuxtLink
+        v-if="excerpt && post.excerpt"
+        :to="post._path"
+        class="border-1 px-4 py-3 border-black border-solid border dark:border-gray-600 rounded-md no-underline hover:bg-gray-100 dark:hover:bg-gray-800"
+      >
+        Read more
+      </NuxtLink>
+    </div>
 
-		<hr />
+    <hr />
 
-		<Shows
-			label="Upcoming Shows"
-			:upcoming="true"
-			direction="asc"
-		/>
+    <Shows
+      label="Upcoming Shows"
+      :upcoming="true"
+      direction="asc"
+    />
 
-		<NuxtLink
-			to="/shows"
-			class="inline-block border-1 mt-2 px-4 py-3 border-black border-solid border dark:border-gray-600 rounded-md no-underline hover:bg-gray-100 dark:hover:bg-gray-800"
-		>
-			See all shows
-		</NuxtLink>
-	</div>
+    <NuxtLink
+      to="/shows"
+      class="inline-block border-1 mt-2 px-4 py-3 border-black border-solid border dark:border-gray-600 rounded-md no-underline hover:bg-gray-100 dark:hover:bg-gray-800"
+    >
+      See all shows
+    </NuxtLink>
+  </div>
 </template>
