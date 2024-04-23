13e30344999d6a38dfb1d17d1db3c1f584f78194
Mark Powell <longlostcousin@gmail.com>
2024-04-23 09:53:51 -0400

    Allow for two ticket vendors

---
+++ components/tickets.vue
@@ -0,0 +1,42 @@
+<script setup>
+  const { show } = defineProps(["show"]);
+
+  const getDomainFromUrl = (url) => {
+    const urlObj = new URL(url);
+    const parts = urlObj.hostname.split(".");
+    const domain = parts.length > 1 ? parts.slice(-2).join(".") : parts[0];
+    return domain;
+  };
+
+  const hasSecondTicketVendor = computed(() => show["ticket-url2"]);
+</script>
+
+<template>
+  <span class="text-sm">
+    <span
+      v-if="hasSecondTicketVendor"
+      class="whitespace-nowrap"
+      >Buy Tickets:
+    </span>
+
+    <a
+      v-if="show['ticket-url']"
+      class="underline block sm:inline-block mr-1"
+      :href="show['ticket-url']"
+      rel="noopener noreferrer"
+    >
+      <template v-if="!hasSecondTicketVendor">Buy Tickets</template>
+      <template v-if="hasSecondTicketVendor">{{
+        getDomainFromUrl(show["ticket-url"])
+      }}</template>
+    </a>
+    <a
+      v-if="hasSecondTicketVendor"
+      class="underline block sm:inline-block"
+      :href="show['ticket-url2']"
+      rel="noopener noreferrer"
+    >
+      {{ getDomainFromUrl(show["ticket-url2"]) }}
+    </a>
+  </span>
+</template>
