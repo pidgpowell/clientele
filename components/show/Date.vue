1bab33d869f4c4d569f5044c8b6b842a07a16006
Mark Powell <longlostcousin@gmail.com>
2024-04-23 11:49:40 -0400

    mobile shows table update

---
+++ components/show/Date.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+  const { date, options } = withDefaults(
+    defineProps<{
+      date: string;
+      options?: { month: string; day: string; year: string };
+    }>(),
+    {
+      options: { month: "short", day: "numeric", year: "numeric" },
+    }
+  );
+
+  const formatDate = useDateFormat();
+</script>
+
+<template>
+  <div class="font-light text-sm/snug">
+    {{ formatDate(date, { month: options.month, day: options.day }) }}
+    <span>, </span>
+    {{ formatDate(date, { year: options.year }) }}
+  </div>
+</template>
---
+++ components/show/Venue.vue
@@ -0,0 +1,12 @@
+<script setup>
+  const { venue } = defineProps(["venue"]);
+  const formatDate = useDateFormat();
+</script>
+
+<template>
+  <div
+    class="text-base/tight sm:text-lg font-semibold sm:font-medium text-gray-700 dark:text-gray-200"
+  >
+    {{ venue }}
+  </div>
+</template>
