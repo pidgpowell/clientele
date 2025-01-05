e76cff61356fff0f475fade038cff12a5b4f67b1
Mark Powell <longlostcousin@gmail.com>
2025-01-05 10:38:25 -0500

    remove experimental flags (#29)

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,11 +12,6 @@ export default defineNuxtConfig({
     domains: ['pbs.twimg.com', 'dispatch-public.s3.amazonaws.com']
   },
 
-  experimental: {
-    payloadExtraction: true,
-    headNext: true
-  },
-
   vue: {
     compilerOptions: {
       isCustomElement: (tag) => ['lite-youtube'].includes(tag),
